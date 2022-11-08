ed2d8cb56064016e90ec1cf9408031a2688530f0
narradorww <rodrigo.anst@gmail.com>
2022-11-07 21:51:47 -0300

    Adicionado o cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dbTrilhas from "./config/conexaoBD.js";
 import trilhas from "./models/Trilha.js";
 import routes from "./routes/index.js";
+import cors from "cors";
 
 
 //conexao com o banco de dados
@@ -12,6 +13,7 @@ dbTrilhas.once("open", () => {
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 routes(app);
